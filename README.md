3e53e59be076bcc1afdf8646f62f4ee07a615f60
Thomas Habets <habets@google.com>
2019-04-17 13:07:36 +0100

    Add copyright things

---
+++ README.md
@@ -0,0 +1,6 @@
+# temp-to-cloud
+
+Uploads temperature sensor data to The Cloud.
+
+This is not an official Google product.
+
--- cmd/temp-to-cloud/screen.go
+++ cmd/temp-to-cloud/screen.go
@@ -1,5 +1,19 @@
 package main
 
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import (
 	"flag"
 	"image"
@@ -46,9 +60,9 @@ func initScreen() (func(), error) {
 
 	i2cConn, err := i2creg.Open(*i2cID)
 	if err != nil {
-		return func(){},err
+		return func() {}, err
 	}
-	iclose := func(){i2cConn.Close()}
+	iclose := func() { i2cConn.Close() }
 
 	if hz != 0 {
 		if err := i2cConn.SetSpeed(hz); err != nil {
